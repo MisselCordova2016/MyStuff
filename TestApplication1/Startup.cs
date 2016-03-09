0d8efc4c8d7251a17221fe970ce679398126e196
MisselCordova2016 <cotler987@gmail.com>
2016-03-09 13:20:03 -0500

    First commit.

---
+++ TestApplication1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestApplication1.Startup))]
+namespace TestApplication1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
